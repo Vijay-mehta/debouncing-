2cc830e419ebda32fbd60c8e0cf69d2aba257431
Vijay-mehta <infovijay1998@gmail.com>
2024-07-26 19:21:43 +0530

    with state

--- app/page.js
+++ app/page.js
@@ -1,32 +1,29 @@
-'use client';
+'use client'
 
-import React from 'react';
+import { useMemo, useState } from "react"
 
-export default function Home() {
-  let count = 0;
+const Home=()=>{
+const [data,setData] =useState('')
+  const fu=(e)=>{
+    console.log("here is api calling.....",e.target.value)
+  }
 
-  const getData = () => {
-    console.log("fetchingData...", count++);
-  };
-
-  const delayFun = (fn, d) => {
+  const handleDebounce =(fu,d)=>{
     let timer;
-    return function (...args) {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        fn.apply(this, args);
-      }, d);
-    };
-  };
+    return function(...arge){
+      clearTimeout(timer)
+   timer=   setTimeout(()=>{
+        fu(...arge)
+      },d)
+    }
+  }
 
-  const debouncing  = delayFun(getData, 3000);
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <label className="mb-4 text-lg font-semibold text-gray-700">
-        Type something in the input field to see the debounce function in action (check console for logs):
-      </label>
-      <input type="text" onChange={debouncing} className="border-2 border-gray-300 px-5 py-2 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
-    </div>
-  );
+  let deBounce = useMemo(()=>handleDebounce(fu,3000),[]) 
+  return(
+    <>
+    <input type="text" value={data} onChange={(e)=>{setData(e.target.value), deBounce(e)}} className=" border-2 px-3 py-3"/>
+    </>
+  )
 }
+
+export default Home;
